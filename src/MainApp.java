a8ba9be1f2c1bca95bb0301df54f22f59ec2db07
Indytskyi <ind.artem2003@gmail.com>
2022-10-27 15:00:37 +0300

    facade pattern

---
+++ src/MainApp.java
@@ -0,0 +1,10 @@
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        BuyCryptoFacade buyCrypto = new BuyCryptoFacade();
+        buyCrypto.buyCryptocurrency(1000, "BTC");
+
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,3 @@
+public record User(String id, String name, double balance, String currency, String accountNbr) {
+
+}
